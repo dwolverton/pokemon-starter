ad64a0e9a4d8a3e450aa2445822cfbb20dce5414
David Wolverton <dwolvert@gmail.com>
2022-07-27 14:37:47 -0400

    first commit

---
+++ src/app/services/pokemon-api.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import {
+  pokemonApiResultToListItem,
+  PokemonApiDetails,
+  PokemonApiResults,
+  pokemonApiDetailsToPokemonDetails,
+} from '../interfaces/pokemon-api-model';
+import { PokemonDetails, PokemonListItem } from '../interfaces/pokemon-model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonApiService {
+  constructor(private http: HttpClient) {}
+
+  fetchAllPokemon(): Observable<PokemonListItem[]> {
+    return this.http
+      .get<PokemonApiResults>('https://pokeapi.co/api/v2/pokemon/?limit=200')
+      .pipe(map((data) => data.results.map(pokemonApiResultToListItem)));
+  }
+
+  fetchPokemonById(id: number): Observable<PokemonDetails> {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+    return this.http
+      .get<PokemonApiDetails>(url)
+      .pipe(map(pokemonApiDetailsToPokemonDetails));
+  }
+}
